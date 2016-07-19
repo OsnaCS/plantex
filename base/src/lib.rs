f151b399d960a2ed24daf59e00a62f678757b906
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-07-19 22:56:52 +0200

    Add a short root module documentation

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,3 +1,9 @@
+//! Functionality used by both the plantex server and the plantex client.
+//!
+//! - the `math` module reexports everything from the `cgmath` crate and
+//!   defines a few own type
+//! - the world module is all about saving and managing the game world
+//!
 extern crate num_traits;
 #[macro_use]
 extern crate log;
