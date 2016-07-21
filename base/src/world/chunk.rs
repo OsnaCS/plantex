06ec6e440fe3cec8c100323af1b1e398bd5a9311
Adrian Richter <adrichter@uos.de>
2016-07-21 16:43:25 +0200

    improve documentation

--- base/src/world/chunk.rs
+++ base/src/world/chunk.rs
@@ -28,7 +28,7 @@ impl Chunk {
         Chunk { pillars: pillars }
     }
 
-    /// Creates a chunk from a Vector<HexPillar>
+    /// Creates a chunk from a `Vec<HexPillar>`
     pub fn from_pillars(pillars: Vec<HexPillar>) -> Self {
         assert_eq!(pillars.len(), CHUNK_SIZE.pow(2));
 
