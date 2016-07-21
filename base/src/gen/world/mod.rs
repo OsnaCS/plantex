f61ecd3719c9152bacbf97618db09efcddcaed36
Adrian Richter <adrichter@uos.de>
2016-07-21 10:38:14 +0200

    implement Provider for WorldGenerator

--- base/src/gen/world/mod.rs
+++ base/src/gen/world/mod.rs
@@ -1,6 +1,8 @@
 //! Procedurally generating the game world.
 //!
 
+use super::super::world::{Chunk, ChunkIndex, Provider};
+
 /// Main type to generate the game world. Implements the `WorldProvider` trait
 /// (TODO, see #8).
 pub struct WorldGenerator {
@@ -16,5 +18,19 @@ impl WorldGenerator {
     /// Returns the seed of this world generator.
     pub fn seed(&self) -> u64 {
         self.seed
+    }
+}
+
+impl Provider for WorldGenerator {
+    /// Returns requested `Chunk`.
+    fn load_chunk(&self, _: ChunkIndex) -> Option<Chunk> {
+        // for the moment returns dummy chunk
+        Some(Chunk::dummy())
+    }
+
+    /// Returns wether `Chunk` load is possible
+    fn is_chunk_loadable(&self, _: ChunkIndex) -> bool {
+        // for the moment returns true
+        true
     }
 }
