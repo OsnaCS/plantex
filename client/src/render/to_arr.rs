02cdb5915a80104a0179f78226d9482737de761a
Jonas Knerr <jkner@uni-osnabrueck.de>
2016-07-21 16:37:04 +0200

    ad the to_arr function for vector and matrix

--- client/src/render/to_arr.rs
+++ client/src/render/to_arr.rs
@@ -7,7 +7,7 @@ pub trait ToArr {
 
     fn to_arr(&self) -> Self::Output;
 }
-
+/// Convert a `Matrix4` into a 4x4 array
 impl<T: BaseNum> ToArr for Matrix4<T> {
     type Output = [[T; 4]; 4];
 
@@ -16,4 +16,47 @@ impl<T: BaseNum> ToArr for Matrix4<T> {
     }
 }
 
-// TODO: Add more impls
+/// Convert a `Matrix3` into a 3x3 array
+impl<T: BaseNum> ToArr for Matrix3<T> {
+    type Output = [[T; 3]; 3];
+
+    fn to_arr(&self) -> Self::Output {
+        (*self).into()
+    }
+}
+
+/// Convert a `Matrix2` into a 2x2 array
+impl<T: BaseNum> ToArr for Matrix2<T> {
+    type Output = [[T; 2]; 2];
+
+    fn to_arr(&self) -> Self::Output {
+        (*self).into()
+    }
+}
+
+/// Convert a `Vector4` into an array
+impl<T: BaseNum> ToArr for Vector4<T> {
+    type Output = [T; 4];
+
+    fn to_arr(&self) -> [T; 4] {
+        (*self).into()
+    }
+}
+
+/// Convert a `Vector3` into an array
+impl<T: BaseNum> ToArr for Vector3<T> {
+    type Output = [T; 3];
+
+    fn to_arr(&self) -> Self::Output {
+        (*self).into()
+    }
+}
+
+/// Convert a `Vector2` into an array
+impl<T: BaseFloat> ToArr for Vector2<T> {
+    type Output = [T; 2];
+
+    fn to_arr(&self) -> Self::Output {
+        (*self).into()
+    }
+}
