754ba33791e4dd5ce788c5163d34fa9b6d6485e2
Karoline Plum <karoline@plum-ge.de>
2016-07-25 14:11:32 +0200

    add terrain generation with open simplex noise

--- base/src/gen/world/mod.rs
+++ base/src/gen/world/mod.rs
@@ -1,21 +1,36 @@
 //! Procedurally generating the game world.
 //!
 
+// extern crate noise;
+
 use world::{CHUNK_SIZE, Chunk, ChunkIndex, ChunkProvider, HeightType, HexPillar};
 use world::{GroundMaterial, PillarSection, Prop, PropType};
 use rand::Rand;
 use gen::PlantGenerator;
+use noise::{PermutationTable, open_simplex2};
+use gen::seeded_rng;
+
+// pub const LANDSCAPE_FLAT: f32 = 0.01;
+pub const LANDSCAPE_MODERATE: f32 = 0.05;
+// pub const LANDSCAPE_HILLY: f32 = 0.1125;
+
 
 /// Main type to generate the game world. Implements the `ChunkProvider` trait
 /// (TODO, see #8).
 pub struct WorldGenerator {
     seed: u64,
+    terrain_table: PermutationTable,
 }
 
 impl WorldGenerator {
     /// Creates the generator with the given seed.
     pub fn with_seed(seed: u64) -> Self {
-        WorldGenerator { seed: seed }
+        let mut rng = seeded_rng(seed, 0, ());
+
+        WorldGenerator {
+            seed: seed,
+            terrain_table: PermutationTable::rand(&mut rng),
+        }
     }
 
     /// Returns the seed of this world generator.
@@ -32,12 +47,15 @@ impl ChunkProvider for WorldGenerator {
 
         for i in q..q + CHUNK_SIZE as i32 {
             for j in r..r + CHUNK_SIZE as i32 {
-                let height = (((i as f32) * 0.25).sin() * 10.0 + ((j as f32) * 0.25).sin() * 10.0 +
-                              100.0) as u16;
 
+                let noise = open_simplex2::<f32>(&self.terrain_table,
+                                                 &[(i as f32) * LANDSCAPE_MODERATE,
+                                                   (j as f32) * LANDSCAPE_MODERATE]);
+                let height = 50.0 + noise * 50.0;
+                println!("{}", height);
                 let ground_section = PillarSection::new(GroundMaterial::Dirt,
                                                         HeightType::from_units(0),
-                                                        HeightType::from_units(height));
+                                                        HeightType::from_units(height as u16));
                 let mut props = Vec::new();
 
                 // Place a test plant every few blocks
@@ -47,7 +65,7 @@ impl ChunkProvider for WorldGenerator {
                     let gen = PlantGenerator::rand(&mut rng);
 
                     props.push(Prop {
-                        baseline: HeightType::from_units(height),
+                        baseline: HeightType::from_units(height as u16),
                         prop: PropType::Plant(gen.generate(&mut rng)),
                     });
                 }
