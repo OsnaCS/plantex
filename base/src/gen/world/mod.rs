3eb3579261621bffdda874029f0e35d29a0c4e49
Karoline Plum <karoline@plum-ge.de>
2016-07-25 16:02:20 +0200

    improve with_pillars() and use it instead of two fors

--- base/src/gen/world/mod.rs
+++ base/src/gen/world/mod.rs
@@ -3,7 +3,7 @@
 
 // extern crate noise;
 
-use world::{CHUNK_SIZE, Chunk, ChunkIndex, ChunkProvider, HeightType, HexPillar};
+use world::{Chunk, ChunkIndex, ChunkProvider, HeightType, HexPillar};
 use world::{GroundMaterial, PillarSection, Prop, PropType};
 use rand::Rand;
 use gen::PlantGenerator;
@@ -25,11 +25,12 @@ pub struct WorldGenerator {
 impl WorldGenerator {
     /// Creates the generator with the given seed.
     pub fn with_seed(seed: u64) -> Self {
-        let mut rng = seeded_rng(seed, 0, ());
+        let mut terrain_rng = seeded_rng(seed, 0, ());
 
         WorldGenerator {
             seed: seed,
-            terrain_table: PermutationTable::rand(&mut rng),
+            terrain_table: PermutationTable::rand(&mut terrain_rng),
+            plant_table: PermutationTable::rand(&mut plant_rng),
         }
     }
 
@@ -41,40 +42,35 @@ impl WorldGenerator {
 
 impl ChunkProvider for WorldGenerator {
     fn load_chunk(&self, index: ChunkIndex) -> Option<Chunk> {
-        let mut pillars = Vec::new();
-        let q = index.0.q * CHUNK_SIZE as i32;
-        let r = index.0.r * CHUNK_SIZE as i32;
+        Some(Chunk::with_pillars(index, |pos| {
+            let real_pos = pos.to_real();
+            let x = real_pos.x;
+            let y = real_pos.y;
 
-        for i in q..q + CHUNK_SIZE as i32 {
-            for j in r..r + CHUNK_SIZE as i32 {
+            let noise = open_simplex2::<f32>(&self.terrain_table,
+                                             &[(x as f32) * LANDSCAPE_MODERATE,
+                                               (y as f32) * LANDSCAPE_MODERATE]);
+            let height = 50.0 + noise * 50.0;
 
-                let noise = open_simplex2::<f32>(&self.terrain_table,
-                                                 &[(i as f32) * LANDSCAPE_MODERATE,
-                                                   (j as f32) * LANDSCAPE_MODERATE]);
-                let height = 50.0 + noise * 50.0;
-                println!("{}", height);
-                let ground_section = PillarSection::new(GroundMaterial::Dirt,
-                                                        HeightType::from_units(0),
-                                                        HeightType::from_units(height as u16));
-                let mut props = Vec::new();
+            let ground_section = PillarSection::new(GroundMaterial::Dirt,
+                                                    HeightType::from_units(0),
+                                                    HeightType::from_units(height as u16));
+            let mut props = Vec::new();
 
-                // Place a test plant every few blocks
-                const TREE_SPACING: i32 = 8;
-                if i % TREE_SPACING == 0 && j % TREE_SPACING == 0 {
-                    let mut rng = super::seeded_rng(self.seed, "TREE", (i, j));
-                    let gen = PlantGenerator::rand(&mut rng);
+            // Place a test plant every few blocks
+            const TREE_SPACING: i32 = 8;
+            if pos.q % TREE_SPACING == 0 && pos.r % TREE_SPACING == 0 {
+                let mut rng = super::seeded_rng(self.seed, "TREE", (pos.q, pos.r));
+                let gen = PlantGenerator::rand(&mut rng);
 
-                    props.push(Prop {
-                        baseline: HeightType::from_units(height as u16),
-                        prop: PropType::Plant(gen.generate(&mut rng)),
-                    });
-                }
-
-                pillars.push(HexPillar::new(vec![ground_section], props));
+                props.push(Prop {
+                    baseline: HeightType::from_units(height as u16),
+                    prop: PropType::Plant(gen.generate(&mut rng)),
+                });
             }
-        }
 
-        Some(Chunk::from_pillars(pillars))
+            HexPillar::new(vec![ground_section], props)
+        }))
     }
 
     fn is_chunk_loadable(&self, _: ChunkIndex) -> bool {
