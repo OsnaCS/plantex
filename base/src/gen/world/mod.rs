ac6408e66e8a917aa346bea321fe41c0edd5f1ff
Karoline Plum <karoline@plum-ge.de>
2016-07-25 17:02:36 +0200

    noise for plants + delete unnecessary comment

--- base/src/gen/world/mod.rs
+++ base/src/gen/world/mod.rs
@@ -1,11 +1,9 @@
 //! Procedurally generating the game world.
 //!
 
-// extern crate noise;
-
 use world::{Chunk, ChunkIndex, ChunkProvider, HeightType, HexPillar};
 use world::{GroundMaterial, PillarSection, Prop, PropType};
-use rand::Rand;
+use rand::{Rand, Rng};
 use gen::PlantGenerator;
 use noise::{PermutationTable, open_simplex2};
 use gen::seeded_rng;
@@ -20,16 +18,19 @@ pub const LANDSCAPE_MODERATE: f32 = 0.05;
 pub struct WorldGenerator {
     seed: u64,
     terrain_table: PermutationTable,
+    plant_table: PermutationTable,
 }
 
 impl WorldGenerator {
     /// Creates the generator with the given seed.
     pub fn with_seed(seed: u64) -> Self {
         let mut terrain_rng = seeded_rng(seed, 0, ());
+        let mut plant_rng = seeded_rng(seed, 1, ());
 
         WorldGenerator {
             seed: seed,
             terrain_table: PermutationTable::rand(&mut terrain_rng),
+            plant_table: PermutationTable::rand(&mut plant_rng),
         }
     }
 
@@ -46,26 +47,30 @@ impl ChunkProvider for WorldGenerator {
             let x = real_pos.x;
             let y = real_pos.y;
 
-            let noise = open_simplex2::<f32>(&self.terrain_table,
-                                             &[(x as f32) * LANDSCAPE_MODERATE,
-                                               (y as f32) * LANDSCAPE_MODERATE]);
-            let height = 50.0 + noise * 50.0;
+            let height_noise = open_simplex2::<f32>(&self.terrain_table,
+                                                    &[(x as f32) * LANDSCAPE_MODERATE,
+                                                      (y as f32) * LANDSCAPE_MODERATE]);
+            let height = 50.0 + height_noise * 50.0;
 
             let ground_section = PillarSection::new(GroundMaterial::Dirt,
                                                     HeightType::from_units(0),
                                                     HeightType::from_units(height as u16));
             let mut props = Vec::new();
 
-            // Place a test plant every few blocks
-            const TREE_SPACING: i32 = 8;
-            if pos.q % TREE_SPACING == 0 && pos.r % TREE_SPACING == 0 {
-                let mut rng = super::seeded_rng(self.seed, "TREE", (pos.q, pos.r));
-                let gen = PlantGenerator::rand(&mut rng);
+            let plant_noise = open_simplex2::<f32>(&self.plant_table,
+                                                   &[(x as f32) * 0.08, (y as f32) * 0.08]);
 
-                props.push(Prop {
-                    baseline: HeightType::from_units(height as u16),
-                    prop: PropType::Plant(gen.generate(&mut rng)),
-                });
+            // Place a test plant every few blocks
+            if plant_noise > 0.0 {
+                let mut rng = super::seeded_rng(self.seed, "TREE", (pos.q, pos.r));
+                if rng.next_f32() < plant_noise {
+                    let gen = PlantGenerator::rand(&mut rng);
+
+                    props.push(Prop {
+                        baseline: HeightType::from_units(height as u16),
+                        prop: PropType::Plant(gen.generate(&mut rng)),
+                    });
+                }
             }
 
             HexPillar::new(vec![ground_section], props)
