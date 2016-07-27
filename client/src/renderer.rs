09c3d4e0da51005c2f59cb8b9009abba3898390a
Lukas Kalde <lkalde@uos.de>
2016-07-27 15:42:17 +0200

    add bloom blur effect

--- client/src/renderer.rs
+++ client/src/renderer.rs
@@ -8,10 +8,11 @@ use std::error::Error;
 use super::weather::Weather;
 use glium::texture::texture2d::Texture2d;
 use glium::texture::{DepthFormat, DepthTexture2d, MipmapsOption, UncompressedFloatFormat};
-use glium::framebuffer::MultiOutputFrameBuffer;
+use glium::framebuffer::{MultiOutputFrameBuffer, SimpleFrameBuffer};
 use glium::{IndexBuffer, Program, VertexBuffer};
 use glium::index::PrimitiveType;
 use glium::backend::Facade;
+use glium::framebuffer::ToColorAttachment;
 
 pub struct Renderer {
     context: Rc<GameContext>,
@@ -21,6 +22,11 @@ pub struct Renderer {
     quad_vertex_buffer: VertexBuffer<Vertex>,
     quad_index_buffer: IndexBuffer<u16>,
     resolution: (u32, u32),
+    bloom_quad_tex: Texture2d,
+    bloom_program: Program,
+    bloom_horz_tex: Texture2d,
+    bloom_vert_tex: Texture2d,
+    bloom_blur_program: Program,
 }
 
 impl Renderer {
@@ -45,6 +51,46 @@ impl Renderer {
                                     &[0u16, 1, 2, 0, 2, 3])
             .unwrap();
 
+        let tonemapping_program = Program::from_source(context.get_facade(),
+                                                       include_str!("tonemapping.vert"),
+                                                       include_str!("tonemapping.frag"),
+                                                       None)
+            .unwrap();
+
+        let bloom_quad_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
+            .unwrap();
+
+        let bloom_program = Program::from_source(context.get_facade(),
+                                                 include_str!("bloom.vert"),
+                                                 include_str!("bloom.frag"),
+                                                 None)
+            .unwrap();
+
+        let bloom_horz_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
+            .unwrap();
+
+        let bloom_vert_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
+            .unwrap();
+
+
+        let bloom_blur_program = Program::from_source(context.get_facade(),
+                                                      include_str!("bloom_blur.vert"),
+                                                      include_str!("bloom_blur.frag"),
+                                                      None)
+            .unwrap();
+
         Renderer {
             context: context.clone(),
             quad_tex: quad_tex_temp,
@@ -53,6 +99,11 @@ impl Renderer {
             quad_vertex_buffer: Renderer::create_vertex_buf(context.get_facade()),
             quad_index_buffer: ibuf,
             resolution: resolution,
+            bloom_quad_tex: bloom_quad_tex,
+            bloom_program: bloom_program,
+            bloom_horz_tex: bloom_horz_tex,
+            bloom_vert_tex: bloom_vert_tex,
+            bloom_blur_program: bloom_blur_program,
         }
     }
 
@@ -83,6 +134,7 @@ impl Renderer {
             .unwrap();
         hdr_buffer.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
+
         world_view.draw(&mut hdr_buffer, camera);
 
         sky_view.draw_skydome(&mut hdr_buffer, camera);
@@ -90,15 +142,119 @@ impl Renderer {
         weather.draw(&mut hdr_buffer, camera);
 
         // ===================================================================
-        // Tonemapping
+        // Creating the Bloom framebuffer
         // ===================================================================
-        let mut target = self.context.get_facade().draw();
-        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+        let debug_show_only_bloom = false;
 
-        let uniforms = uniform! {
+        let mut uniforms = uniform! {
             decal_texture: &self.quad_tex,
             exposure: 1.0f32
         };
+
+        let mut bloom_buffer = SimpleFrameBuffer::with_depth_buffer(self.context.get_facade(),
+                                                                    self.bloom_quad_tex
+                                                                        .to_color_attachment(),
+                                                                    &self.depth_texture)
+            .unwrap();
+
+        bloom_buffer.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+        bloom_buffer.draw(&self.quad_vertex_buffer,
+                  &self.quad_index_buffer,
+                  &self.bloom_program,
+                  &uniforms,
+                  &Default::default())
+            .unwrap();
+
+
+
+        // prepare blur
+
+        let mut bloom_blur_horz_buffer = SimpleFrameBuffer::new(self.context.get_facade(),
+                                                                self.bloom_horz_tex
+                                                                    .to_color_attachment())
+            .unwrap();
+        bloom_blur_horz_buffer.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+
+        let mut bloom_blur_vert_buffer = SimpleFrameBuffer::new(self.context.get_facade(),
+                                                                self.bloom_vert_tex
+                                                                    .to_color_attachment())
+            .unwrap();
+        bloom_blur_vert_buffer.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+        let mut uniforms_horz_blur = uniform! {
+            //for the first iteration: Use the bloom quad texture, from second iteration on this will change to bloom_vert_tex
+            image: &self.bloom_quad_tex,
+            horizontal: true,
+        };
+        let uniforms_vert_blur = uniform! {
+            image: &self.bloom_horz_tex,
+            horizontal: false,
+        };
+
+
+        let mut first_iteration = true; //to know when we need to switch uniforms_horz_blur
+        let mut horizontal = true;      //to switch between horizontal and vertical blur
+
+
+        for _ in 0..10 {
+            if horizontal {
+                bloom_blur_horz_buffer.draw(&self.quad_vertex_buffer,
+                          &self.quad_index_buffer,
+                          &self.bloom_blur_program,
+                          &uniforms_horz_blur,
+                          &Default::default())
+                    .unwrap();
+            } else {
+                bloom_blur_vert_buffer.draw(&self.quad_vertex_buffer,
+                          &self.quad_index_buffer,
+                          &self.bloom_blur_program,
+                          &uniforms_vert_blur,
+                          &Default::default())
+                    .unwrap();
+            }
+            if first_iteration {
+                uniforms_horz_blur = uniform! {
+                    image: &self.bloom_vert_tex,
+                    horizontal: true,
+                };
+                first_iteration = false;
+            }
+            horizontal = !horizontal;
+        }
+
+
+
+
+        // ===================================================================
+        // Tonemapping
+        // ===================================================================
+
+        if debug_show_only_bloom {
+            if horizontal {
+                uniforms = uniform! {
+                    decal_texture: &self.bloom_vert_tex,
+                    exposure: 1.0f32
+                };
+            } else {
+                uniforms = uniform! {
+                    decal_texture: &self.bloom_horz_tex,
+                    exposure: 1.0f32
+                };
+
+            }
+        } else {
+            uniforms = uniform! {
+                decal_texture: &self.quad_tex,
+                exposure: 1.0f32
+            };
+
+        };
+
+
+        let mut target = self.context.get_facade().draw();
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
 
         target.draw(&self.quad_vertex_buffer,
                   &self.quad_index_buffer,
@@ -154,6 +310,34 @@ impl Renderer {
                                                                MipmapsOption::NoMipmap,
                                                                self.resolution.0,
                                                                self.resolution.1)
+            .unwrap();
+
+
+        self.bloom_quad_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
+            .unwrap();
+
+        self.bloom_program = Program::from_source(context.get_facade(),
+                                                 include_str!("bloom.vert"),
+                                                 include_str!("bloom.frag"),
+                                                 None)
+            .unwrap();
+
+        self.bloom_horz_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
+            .unwrap();
+
+        self.bloom_vert_tex = Texture2d::empty_with_format(context.get_facade(),
+                                                          UncompressedFloatFormat::F32F32F32F32,
+                                                          MipmapsOption::NoMipmap,
+                                                          resolution.0,
+                                                          resolution.1)
             .unwrap();
     }
 }
