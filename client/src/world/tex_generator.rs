60a6170870fa5d48d72706d976687ecd71204b8c
Helena Keller <helenakeller92@gmail.com>
2016-08-01 11:36:49 +0200

    Height maps for grass, sand and snow

--- client/src/world/tex_generator.rs
+++ client/src/world/tex_generator.rs
@@ -17,3 +17,55 @@ pub fn create_noise(seed: u64) -> Vec<Vec<(f32, f32, f32)>> {
     }
     v
 }
+
+pub fn create_height_map_for_snow(seed: u64) -> Vec<Vec<f32>> {
+    let mut texture_rng = seeded_rng(seed, 13, ());
+    let table = PermutationTable::rand(&mut texture_rng);
+    let mut snow_height_map = vec![Vec::new(); 256];
+
+    for i in 0..256 {
+        for j in 0..256 {
+            let e = ((open_simplex2::<f32>(&table, &[(i as f32), (j as f32)]) + 1.0) / 2.0) +
+                    0.5 *
+                    ((open_simplex2::<f32>(&table, &[(i as f32) * 2.0, (j as f32) * 2.0]) +
+                      1.0) / 2.0) +
+                    0.25 *
+                    ((open_simplex2::<f32>(&table, &[(i as f32) * 4.0, (j as f32) * 4.0]) + 1.0) /
+                     2.0);
+            snow_height_map[i].push(e.powf(0.35));
+        }
+    }
+    snow_height_map
+}
+
+pub fn create_height_map_for_sand(seed: u64) -> Vec<Vec<f32>> {
+    let mut texture_rng = seeded_rng(seed, 13, ());
+    let table = PermutationTable::rand(&mut texture_rng);
+    let mut sand_height_map = vec![Vec::new(); 256];
+
+    for i in 0..256 {
+        for j in 0..256 {
+            let e = (open_simplex2::<f32>(&table, &[(i as f32) * 1.14, (j as f32) * 1.14]) + 1.0) /
+                    2.0;
+            sand_height_map[i].push(e);
+        }
+    }
+    sand_height_map
+}
+
+pub fn create_height_map_for_grass(seed: u64) -> Vec<Vec<f32>> {
+    let mut texture_rng = seeded_rng(seed, 13, ());
+    let table = PermutationTable::rand(&mut texture_rng);
+    let mut grass_height_map = vec![Vec::new(); 256];
+
+    for i in 0..256 {
+        for j in 0..256 {
+            let e = ((open_simplex2::<f32>(&table, &[(i as f32) * 7.0, (j as f32)]) + 7.0) / 2.0) +
+                    0.5 *
+                    ((open_simplex2::<f32>(&table, &[(i as f32) * 9.0, (j as f32) * 9.0]) + 1.0) /
+                     2.0);
+            grass_height_map[i].push(e.powf(0.35));
+        }
+    }
+    grass_height_map
+}
