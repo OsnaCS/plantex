1d79d1c966eb22815e15c0348ac41e6aec8b6707
jonas-schievink <jonas@schievink.net>
2016-08-02 19:01:05 +0200

    fmt

--- client/src/view/plant_renderer.rs
+++ client/src/view/plant_renderer.rs
@@ -8,9 +8,7 @@ pub struct PlantRenderer {
 
 impl PlantRenderer {
     pub fn new(context: Rc<GameContext>) -> Self {
-        PlantRenderer {
-            program: context.load_program("plants").unwrap(),
-        }
+        PlantRenderer { program: context.load_program("plants").unwrap() }
     }
 
     pub fn program(&self) -> &Program {
