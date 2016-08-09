1a25ab60f352a7a4d098e656649c11aed9890536
vab9 <vbrinkhege@uos.de>
2016-08-10 00:47:43 +0200

    testing travis #3

--- ci/install-glslangValidator.sh
+++ ci/install-glslangValidator.sh
@@ -4,7 +4,7 @@ set -e
 echo "need to install glslangValidator..."
 mkdir validator/
 cd validator/
-wget "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Linux/glslangValidator"
+wget --no-check-certificate "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Linux/glslangValidator"
 chmod +x glslangValidator
 cd ../
 echo "installed glslangValidator to $PWD/validator/glslangValidator"
